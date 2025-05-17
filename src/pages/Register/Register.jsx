cade40bfe018d5f5b998c93187aecd2e1005e61a
Noor Piash <noorpiash1234@gmail.com>
2025-05-18 02:25:41 +0600

    firebase-auth

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,11 +1,39 @@
 import Lottie from 'lottie-react'
 import registerLottieData from "../../assets/lottie files/register.json.json"
 
+import AuthContext from '../../context/AuthContext/AuthContext'
+import { useContext } from 'react';
+
 const Register = () => {
+
+    const { createUser } = useContext(AuthContext);
 
     const handleRegister = e => {
         e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+
+        createUser(email, password)
+            .then(result => {
+                console.log(result.user)
+            })
+            .catch(error => {
+                console.error(error.message);
+            })
+
+        const passwordRegex = /^(?=.*[!@#$%^&*()_+[\]{};':"\\|,.<>/?]).{8,}$/;
+
+        if (!passwordRegex.test(password)) {
+            alert("Password must be at least 8 characters long and include at least one special character.");
+            return;
+        }
     }
+
+
+
+
 
     return (
         <div className="hero bg-base-200 min-h-screen">
@@ -17,14 +45,15 @@ const Register = () => {
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                     <div className="card-body">
                         <h1 className="text-5xl font-bold">Register now!</h1>
-                        <fieldset onSubmit={handleRegister} className="fieldset">
+                        <form onSubmit={handleRegister} action=""> <fieldset className="fieldset">
                             <label className="label">Email</label>
                             <input type="email" name='email' className="input" placeholder="Email" />
                             <label className="label">Password</label>
                             <input type="password" name='password' className="input" placeholder="Password" />
                             <div><a className="link link-hover">Forgot password?</a></div>
                             <button className="btn btn-neutral mt-4">Register</button>
-                        </fieldset>
+                        </fieldset></form>
+
                     </div>
                 </div>
             </div>
