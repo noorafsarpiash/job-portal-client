24d0d99b20952843bd37c15938f73398ff21f4bc
Noor Piash <noorpiash1234@gmail.com>
2025-06-06 01:21:31 +0600

    jod-application

--- src/pages/JobApply/JobApply.jsx
+++ src/pages/JobApply/JobApply.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import useAuth from '../../hooks/useAuth';
+import Swal from 'sweetalert2';
+
 
 const JobApply = () => {
     const { id } = useParams();
-    console.log(id);
+    const { user } = useAuth();
+    console.log(id, user);
 
     const submitJobApplication = e => {
         e.preventDefault();
@@ -13,36 +17,60 @@ const JobApply = () => {
         const resume = form.resume.value;
 
         console.log(linkedin, github, resume);
+
+        const jobApplication = {
+            job_id: id,
+            application_email: user.email,
+            linkedin,
+            github,
+            resume
+        }
+
+        fetch("http://localhost:5000/job-application", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(jobApplication)
+        })
+
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "Your work has been saved",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+            })
+
+
     }
 
     return (
-        <div className="hero bg-base-200 min-h-screen">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
-                    <p className="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
-                    </p>
-                </div>
-                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                    <div className="card-body">
-                        <form onSubmit={submitJobApplication}>
-                            <fieldset className="fieldset">
-                                <label className="label">LinkedIn URL</label>
-                                <input type="url" className="input" name='linkedin' placeholder="LinkedIn URL" />
-                                <label className="label">GitHub URL</label>
-                                <input type="url" className="input" name='github' placeholder="GitHub URL" />
-                                <label className="label">Resume URL</label>
-                                <input type="url" className="input" name='resume' placeholder="Resume URL" />
 
-                                <button className="btn btn-neutral mt-4">Apply</button>
-                            </fieldset>
-                        </form>
-                    </div>
+        <div className="card bg-base-100 w-full shadow-2xl">
+
+            <h1 className="text-5xl font-bold text-center">Apply Job and Good Luck Now!</h1>
+            <form onSubmit={submitJobApplication} className="card-body ">
+                <div className='form-control '>
+                    <fieldset className="fieldset ">
+                        <label className="label">LinkedIn URL</label>
+                        <input type="url" className="input w-full" name='linkedin' placeholder="LinkedIn URL" />
+                        <label className="label">GitHub URL</label>
+                        <input type="url" className="input w-full" name='github' placeholder="GitHub URL" />
+                        <label className="label">Resume URL</label>
+                        <input type="url" className="input w-full" name='resume' placeholder="Resume URL" />
+
+                        <button className="btn btn-neutral mt-4">Apply</button>
+                    </fieldset>
                 </div>
-            </div>
+            </form>
         </div>
+
     )
 }
 
